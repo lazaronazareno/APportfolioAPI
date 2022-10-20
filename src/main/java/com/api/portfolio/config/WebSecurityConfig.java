c89312ed0c4ace3dbe5c22f7b3f449cfb7f9ed65
lazaronazareno <lazaronazareno@gmail.com>
2022-10-20 16:55:44 -0300

    httpsecurity cors

--- src/main/java/com/api/portfolio/config/WebSecurityConfig.java
+++ src/main/java/com/api/portfolio/config/WebSecurityConfig.java
@@ -21,9 +21,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  *
@@ -62,8 +59,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
             // We don't need CSRF for this example
+            httpSecurity.cors().and().csrf().disable()
                             // dont authenticate this particular request
-                            httpSecurity.antMatcher("/**").authorizeRequests().antMatchers("/authenticate", "/register").permitAll().
+                            .authorizeRequests().antMatchers("/authenticate", "/register").permitAll().
                     
                             // all other requests need to be authenticated
                     
@@ -73,10 +71,22 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                             exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
                             .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
             
-            httpSecurity.cors().and().csrf().disable()
-;
-
             // Add a filter to validate the tokens with every request
             httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
     }
+    
+    @Bean
+        public CorsConfigurationSource corsConfigurationSource() {
+            final CorsConfiguration config = new CorsConfiguration();
+
+            config.setAllowedOrigins(Arrays.asList("https://argentina-programa-back-end.herokuapp.com"));
+            config.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS", "DELETE", "PUT", "PATCH"));
+            config.setAllowCredentials(true);
+            config.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+
+            final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+            source.registerCorsConfiguration("/**", config);
+
+            return source;
+        }
 }
